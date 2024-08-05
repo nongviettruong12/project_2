6832f596725dee6e70451f88f46adb5e803cca12
1ongtimenosee <nongviettruong12@gmail.com>
2024-08-05 10:17:59 +0700

    "feat": config table and create db.json and fetch api

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,108 +1,315 @@
-import React from 'react';
-import { Table, Button, Tag } from 'antd';
-import { SettingOutlined } from '@ant-design/icons';
+import { useEffect, useState } from 'react';
+import { EllipsisOutlined, PlusOutlined } from '@ant-design/icons';
+import { ProTable, TableDropdown } from '@ant-design/pro-components';
+import { Button, Dropdown, Space, Tag } from 'antd';
+import { useRef } from 'react';
+export const waitTimePromise = async (time= 100) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(true);
+    }, time);
+  });
+};
 
-const dataSource = [
-  {
-    key: '441',
-    status: 'Đang tập sự',
-    name: 'VŨ THẾ HOA',
-    birthDate: '12/12/2001',
-    internshipYear: '2024',
-    idNumber: '112233445566',
-    idIssueDate: '07/03/2024',
-    organization: '2',
-    certificate: '123',
-    duration: ''
-  },
-  {
-    key: '442',
-    status: 'Đang tập sự',
-    name: 'ALO',
-    birthDate: '07/12/2001',
-    internshipYear: '2024',
-    idNumber: '123123123123',
-    idIssueDate: '21/03/2024',
-    organization: '2',
-    certificate: '123',
-    duration: ''
-  },
-];
+export const waitTime = async (time = 100) => {
+  await waitTimePromise(time);
+};
 
-const columns = [
+  
+
+
+
+const columns  = [
   {
     title: 'STT',
-    dataIndex: 'key',
-    key: 'key',
+    dataIndex: 'index',
+    valueType: 'indexBorder',
+    width: 48,
   },
   {
     title: 'Trạng thái',
     dataIndex: 'status',
-    key: 'status',
-    render: status => (
-      <Tag color="yellow">
-        {status}
-      </Tag>
-    ),
+    copyable: true,
+    ellipsis: true,
+    sorter: true,
+    hideInSearch: true,
+    formItemProps: {
+      rules: [
+        {
+          required: true,
+          message: '此项为必填项',
+        },
+      ],
+    },
   },
   {
     title: 'Họ và tên',
-    dataIndex: 'name',
-    key: 'name',
+    dataIndex: 'fullName',
+    copyable: true,
+    ellipsis: true,
+    sorter: true,
+    hideInSearch: true,
+    formItemProps: {
+      rules: [
+        {
+          required: true,
+          message: '此项为必填项',
+        },
+      ],
+    },
   },
   {
     title: 'Ngày sinh',
-    dataIndex: 'birthDate',
-    key: 'birthDate',
+    dataIndex: 'birthDay',
+    copyable: true,
+    ellipsis: true,
+    sorter: true,
+    hideInSearch: true,
+    formItemProps: {
+      rules: [
+        {
+          required: true,
+          message: '此项为必填项',
+        },
+      ],
+    },
   },
   {
-    title: 'Năm Tập sự',
-    dataIndex: 'internshipYear',
-    key: 'internshipYear',
+    title: 'Ngày  thuc tap',
+    dataIndex: 'probationDay',
+    sorter: true,
+    hideInSearch: true,
+    copyable: true,
+    ellipsis: true,
+    formItemProps: {
+      rules: [
+        {
+          required: true,
+          message: '此项为必填项',
+        },
+      ],
+    },
   },
   {
-    title: 'Số CCCD',
-    dataIndex: 'idNumber',
-    key: 'idNumber',
+    title: 'nam  thuc tap',
+    dataIndex: 'probationYear',
+    sorter: true,
+    hideInSearch: true,
+    copyable: true,
+    ellipsis: true,
+    formItemProps: {
+      rules: [
+        {
+          required: true,
+          message: '此项为必填项',
+        },
+      ],
+    },
   },
   {
-    title: 'Ngày cấp CCCD',
-    dataIndex: 'idIssueDate',
-    key: 'idIssueDate',
+    title: 'so cccd',
+    dataIndex: 'identifyNumber',
+    sorter: true,
+    hideInSearch: true,
+    copyable: true,
+    ellipsis: true,
+    formItemProps: {
+      rules: [
+        {
+          required: true,
+          message: '此项为必填项',
+        },
+      ],
+    },
   },
   {
-    title: 'Tổ chức hành nghề',
-    dataIndex: 'organization',
-    key: 'organization',
+    title: 'ngay cap',
+    dataIndex: 'identifyDay',
+    sorter: true,
+    hideInSearch: true,
+    copyable: true,
+    ellipsis: true,
+    formItemProps: {
+      rules: [
+        {
+          required: true,
+          message: '此项为必填项',
+        },
+      ],
+    },
   },
   {
-    title: 'Chứng chỉ',
+    title: 'chung chi',
     dataIndex: 'certificate',
-    key: 'certificate',
+    sorter: true,
+    hideInSearch: true,
+    copyable: true,
+    ellipsis: true,
+    formItemProps: {
+      rules: [
+        {
+          required: true,
+          message: '此项为必填项',
+        },
+      ],
+    },
   },
   {
-    title: 'Thời gian học kéo dài',
-    dataIndex: 'duration',
-    key: 'duration',
+    title: 'thời gian học',
+    dataIndex: 'TimeOfLearning',
+    sorter: true,
+    hideInSearch: true,
+    copyable: true,
+    ellipsis: true,
+    formItemProps: {
+      rules: [
+        {
+          required: true,
+          message: '此项为必填项',
+        },
+      ],
+    },
   },
   {
-    title: '',
-    key: 'action',
-    render: () => (
-      <Button icon={<SettingOutlined />} />
-    ),
+    title: '创建时间',
+    dataIndex: 'created_at',
+    valueType: 'dateRange',
+    hideInTable: true,
+    search: {
+      transform: (value) => {
+        return {
+          startTime: value[0],
+          endTime: value[1],
+        };
+      },
+    },
+  },
+  {
+    title: 'hành động',
+    valueType: 'option',
+    key: 'option',
+    render: (text, record, _, action) => [
+      <a
+        key="editable"
+        onClick={() => {
+          action?.startEditable?.(record.id);
+        }}
+      >
+        edit
+      </a>,
+      <TableDropdown
+        key="actionGroup"
+        onSelect={() => action?.reload()}
+        menus={[
+          { key: 'copy', name: 'copy' },
+          { key: 'delete', name: 'xóa' },
+        ]}
+      />,
+    ],
   },
 ];
 
-const MyTable = () => (
-  <Table
-    dataSource={dataSource}
-    columns={columns}
-    pagination={{ pageSize: 20, total: 446, showQuickJumper: true, showSizeChanger: true }}
-    bordered
-    title={() => 'Danh sách tập sự (446)'}
-    footer={() => '© Your Company'}
-  />
-);
+const MyTable = () => {
+  const [dataSource, setDataSource] = useState([])
 
-export default MyTable;
+  useEffect(() => {
+    fetch('http://localhost:3000/user')
+    .then((res) =>res.json())
+    .then((data) => {
+      setDataSource(data);
+      console.log(data);
+    })
+  }, []);
+  const actionRef = useRef();
+  return (
+    <ProTable
+      columns={columns}
+      actionRef={actionRef}
+      cardBordered
+      // request={async (params, sort, filter) => {
+      //   console.log(sort, filter);
+      //   await waitTime(2000);
+      //   return request<{
+      //   }>('https://proapi.azurewebsites.net/github/issues', {
+      //     params,
+      //   });
+      // }}
+      dataSource={dataSource}
+      editable={{
+        type: 'multiple',
+      }}
+      columnsState={{
+        persistenceKey: 'pro-table-singe-demos',
+        persistenceType: 'localStorage',
+        defaultValue: {
+          option: { fixed: 'right', disable: true },
+        },
+        onChange(value) {
+          console.log('value: ', value);
+        },
+      }}
+      rowKey="id"
+      search={{
+        labelWidth: 'auto',
+      }}
+      options={{
+        setting: {
+          listsHeight: 400,
+        },
+      }}
+      form={{
+        syncToUrl: (values, type) => {
+          if (type === 'get') {
+            return {
+              ...values,
+              created_at: [values.startTime, values.endTime],
+            };
+          }
+          return values;
+        },
+      }}
+      pagination={{
+        pageSize: 5,
+        onChange: (page) => console.log(page),
+      }}
+      dateFormatter="string"
+      headerTitle="Danh sách tập sự"
+      toolBarRender={() => [
+        <Button
+          key="button"
+          icon={<PlusOutlined />}
+          onClick={() => {
+            actionRef.current?.reload();
+          }}
+          type="primary"
+        >
+          add
+        </Button>,
+        <Dropdown
+          key="menu"
+          menu={{
+            items: [
+              {
+                label: '1st item',
+                key: '1',
+              },
+              {
+                label: '2nd item',
+                key: '2',
+              },
+              {
+                label: '3rd item',
+                key: '3',
+              },
+            ],
+          }}
+        >
+          <Button>
+            <EllipsisOutlined />
+          </Button>
+        </Dropdown>,
+      ]}
+    />
+  );
+};
+export default MyTable
